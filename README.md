60aa3be4920afbf168efabd905c375d8ea42f0eb
Dev2vinod <vinodsinghfun@gmail.com>
2024-07-31 09:58:46 +0530

    complete counter app
    good counter app with good ui

---
+++ README.md
@@ -0,0 +1,4 @@
+# React + Vite
+
+1. i have made the counter app in counter file 
+2. it has good ui 
---
+++ src/Counter.jsx
@@ -0,0 +1,41 @@
+
+    import React, { useState } from 'react'
+    
+    const Counter = () => {
+
+         const[count,setCount] = useState(0);
+
+         function hello(){
+            // console.log("hello ")
+            setCount(count+1)
+         }
+
+         function sad(){
+            setCount(count-1)
+         }
+
+      return (
+        <>
+
+        <div className='bg-gray-600 text-lime-400 flex mx-auto items-center  flex-col w-[50%] my-9 rounded shadow-lg p-2 '>
+
+        <div>  A simple   Counter</div>
+
+            <div>
+                <p className='text-3xl font-bold'>   {count} </p>
+            </div>
+
+            <div>
+
+         <button onClick={hello} className='bg-blue-500 hover:bg-blue-400 px-6 rounded-lg m-3' >+</button>
+
+         <button onClick={sad}className='bg-green-500 hover:bg-green-400 px-6 rounded-lg' >-</button>
+
+            </div>
+        </div>
+
+        </>
+      )
+    }
+    
+    export default Counter
